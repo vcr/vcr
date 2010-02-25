57c67f3844711f8ab93f733acf4718b7269307ae
Myron Marston <myron.marston@gmail.com>
2010-02-24 23:25:38 -0800

    Require rubygems in spec_helper.rb.  This is needed in order to run the specs on ruby 1.8.x.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'rubygems'
 require 'vcr'
 require 'spec'
 require 'spec/autorun'
