8fe911f52fbf76cc20f97ffe7d3027a344e38055
Myron Marston <myron.marston@gmail.com>
2010-04-11 10:04:50 -0700

    Removed unneeded fakeweb extensions.

--- lib/vcr/extensions/fake_web.rb
+++ lib/vcr/extensions/fake_web.rb
@@ -1,20 +1,6 @@
 require 'fakeweb'
 
 module FakeWeb
-  def self.remove_from_registry(method, url)
-    Registry.instance.remove(method, url)
-  end
-
-  def self.with_allow_net_connect_set_to(value)
-    original_value = FakeWeb.allow_net_connect?
-    begin
-      FakeWeb.allow_net_connect = value
-      yield
-    ensure
-      FakeWeb.allow_net_connect = original_value
-    end
-  end
-
   def self.request_uri(net_http, request)
     # Copied from: http://github.com/chrisk/fakeweb/blob/fakeweb-1.2.8/lib/fake_web/ext/net_http.rb#L39-52
     protocol = net_http.use_ssl? ? "https" : "http"
@@ -30,16 +16,5 @@ module FakeWeb
     end
 
     "#{protocol}://#{userinfo}#{net_http.address}:#{net_http.port}#{path}"
-  end
-
-  class Registry #:nodoc:
-    def remove(method, url)
-      uri_map.delete_if do |uri, method_hash|
-        if normalize_uri(uri) == normalize_uri(url)
-          method_hash.delete(method)
-          method_hash.empty? # there's no point in keeping this entry in the uri map if its method hash is empty...
-        end
-      end
-    end
   end
 end
