f9102cf3d7ac5e27b32f9f5ef3d5266925a77b92
Myron Marston <myron.marston@gmail.com>
2010-06-11 16:36:45 -0700

    Moved code out of fakeweb extension and into fakeweb http_stubbing_adapter.

--- lib/vcr/http_stubbing_adapters/fakeweb.rb
+++ lib/vcr/http_stubbing_adapters/fakeweb.rb
@@ -1,4 +1,4 @@
-require 'vcr/extensions/fake_web'
+require 'fakeweb'
 require 'vcr/extensions/net_http'
 
 module VCR
@@ -44,7 +44,20 @@ module VCR
         end
 
         def request_uri(net_http, request)
-          ::FakeWeb.request_uri(net_http, request)
+          # Copied from: http://github.com/chrisk/fakeweb/blob/fakeweb-1.2.8/lib/fake_web/ext/net_http.rb#L39-52
+          protocol = net_http.use_ssl? ? "https" : "http"
+
+          path = request.path
+          path = URI.parse(request.path).request_uri if request.path =~ /^http/
+
+          if request["authorization"] =~ /^Basic /
+            userinfo = FakeWeb::Utility.decode_userinfo_from_header(request["authorization"])
+            userinfo = FakeWeb::Utility.encode_unsafe_chars_in_userinfo(userinfo) + "@"
+          else
+            userinfo = ""
+          end
+
+          "#{protocol}://#{userinfo}#{net_http.address}:#{net_http.port}#{path}"
         end
 
         private
