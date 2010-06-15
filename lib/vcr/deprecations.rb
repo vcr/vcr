2c7983fc7b5e283589ff43bd9953fd1450a08709
Myron Marston <myron.marston@gmail.com>
2010-06-15 10:13:25 -0700

    Remove old deprecations.  The next release will be VCR 1.0.0, indicating breaking changes.

--- lib/vcr/deprecations.rb
+++ lib/vcr/deprecations.rb
@@ -1,30 +1,5 @@
 module VCR
-  def create_cassette!(*args)
-    warn "WARNING: VCR.create_cassette! is deprecated.  Instead, use: VCR.insert_cassette."
-    insert_cassette(*args)
-  end
-
-  def destroy_cassette!(*args)
-    warn "WARNING: VCR.destroy_cassette! is deprecated.  Instead, use: VCR.eject_cassette."
-    eject_cassette(*args)
-  end
-
-  def with_cassette(*args, &block)
-    warn "WARNING: VCR.with_cassette is deprecated.  Instead, use: VCR.use_cassette."
-    use_cassette(*args, &block)
-  end
-
   class Cassette
-    def destroy!(*args)
-      warn "WARNING: VCR::Cassette#destroy! is deprecated.  Instead, use: VCR::Cassette#eject."
-      eject(*args)
-    end
-
-    def cache_file(*args)
-      warn "WARNING: VCR::Cassette#cache_file is deprecated.  Instead, use: VCR::Cassette#file."
-      file(*args)
-    end
-
     def allow_real_http_requests_to?(uri)
       warn "WARNING: VCR::Cassette#allow_real_http_requests_to? is deprecated and should no longer be used."
       VCR.http_stubbing_adapter.ignore_localhost && %w(localhost 127.0.0.1).include?(uri.host)
@@ -33,11 +8,6 @@ module VCR
     private
 
     def deprecate_old_cassette_options(options)
-      if @record_mode == :unregistered
-        @record_mode = :new_episodes
-        Kernel.warn "WARNING: VCR's :unregistered record mode is deprecated.  Instead, use: :new_episodes."
-      end
-
       message = "VCR's :allow_real_http cassette option is deprecated.  Instead, use the ignore_localhost configuration option."
       if options[:allow_real_http] == :localhost
         @original_ignore_localhost = VCR.http_stubbing_adapter.ignore_localhost
@@ -52,23 +22,6 @@ module VCR
       if defined?(@original_ignore_localhost)
         VCR.http_stubbing_adapter.ignore_localhost = @original_ignore_localhost
       end
-    end
-  end
-
-  class Config
-    def self.cache_dir(*args)
-      warn "WARNING: VCR::Config.cache_dir is deprecated.  Instead, use: VCR::Config.cassette_library_dir."
-      cassette_library_dir(*args)
-    end
-
-    def self.cache_dir=(value)
-      warn "WARNING: VCR::Config.cache_dir= is deprecated.  Instead, use: VCR::Config.cassette_library_dir=."
-      self.cassette_library_dir = value
-    end
-
-    def self.default_cassette_record_mode=(value)
-      warn %Q{WARNING: #default_cassette_record_mode is deprecated.  Instead, use: "default_cassette_options = { :record => :#{value.to_s} }"}
-      default_cassette_options.merge!(:record => value)
     end
   end
 end
--- spec/deprecations_spec.rb
+++ spec/deprecations_spec.rb
@@ -1,28 +1,9 @@
 require 'spec_helper'
 
 describe 'Deprecations' do
-  describe VCR do
-    subject { VCR }
-    deprecated :create_cassette!,  :insert_cassette, "WARNING: VCR.create_cassette! is deprecated.  Instead, use: VCR.insert_cassette."
-    deprecated :destroy_cassette!, :eject_cassette,  "WARNING: VCR.destroy_cassette! is deprecated.  Instead, use: VCR.eject_cassette."
-    deprecated :with_cassette,     :use_cassette,    "WARNING: VCR.with_cassette is deprecated.  Instead, use: VCR.use_cassette."
-  end
-
   describe VCR::Cassette do
     disable_warnings
     subject { VCR::Cassette.new('cassette name') }
-    deprecated :destroy!,   :eject, "WARNING: VCR::Cassette#destroy! is deprecated.  Instead, use: VCR::Cassette#eject."
-    deprecated :cache_file, :file,  "WARNING: VCR::Cassette#cache_file is deprecated.  Instead, use: VCR::Cassette#file."
-
-    it 'delegates the :unregistered record option to :new_episodes' do
-      cassette = VCR::Cassette.new('cassette name', :record => :unregistered)
-      cassette.record_mode.should == :new_episodes
-    end
-
-    it "prints a warning: WARNING: VCR's :unregistered record mode is deprecated.  Instead, use: :new_episodes." do
-      Kernel.should_receive(:warn).with("WARNING: VCR's :unregistered record mode is deprecated.  Instead, use: :new_episodes.")
-      VCR::Cassette.new('cassette name', :record => :unregistered)
-    end
 
     it 'raises an error when an :allow_real_http lambda is given' do
       expect { VCR::Cassette.new('cassette name', :allow_real_http => lambda {}) }.to raise_error(ArgumentError)
@@ -64,46 +45,6 @@ describe 'Deprecations' do
               subject.allow_real_http_requests_to?(URI.parse(url)).should == expected_value
             end
           end
-        end
-      end
-    end
-  end
-
-  describe VCR::Config do
-    disable_warnings
-    subject { VCR::Config }
-    deprecated :cache_dir,  :cassette_library_dir,  "WARNING: VCR::Config.cache_dir is deprecated.  Instead, use: VCR::Config.cassette_library_dir."
-
-    it 'delegates #cache_dir= to #cassette_library_dir=' do
-      subject.should_receive(:cassette_library_dir=).with(:value)
-      subject.cache_dir = :value
-    end
-
-    it "prints a warning: WARNING: VCR::Config.cache_dir= is deprecated.  Instead, use: VCR::Config.cassette_library_dir=." do
-      subject.stub!(:cassette_library_dir=)
-      subject.should_receive(:warn).with("WARNING: VCR::Config.cache_dir= is deprecated.  Instead, use: VCR::Config.cassette_library_dir=.")
-      subject.cache_dir = :value
-    end
-
-    describe '#default_cassette_record_mode=' do
-      it 'sets the default_cassette_options[:record] option' do
-        VCR::Cassette::VALID_RECORD_MODES.each do |mode|
-          VCR::Config.default_cassette_options = nil
-          VCR::Config.default_cassette_record_mode = mode
-          VCR::Config.default_cassette_options[:record].should == mode
-        end
-      end
-
-      it 'merges the :record option with the existing default_cassette_record options' do
-        VCR::Config.default_cassette_options = { :an => :option }
-        VCR::Config.default_cassette_record_mode = :all
-        VCR::Config.default_cassette_options.should == { :an => :option, :record => :all }
-      end
-
-      it 'warns the user that it is deprecated' do
-        VCR::Cassette::VALID_RECORD_MODES.each do |mode|
-          VCR::Config.should_receive(:warn).with(%Q{WARNING: #default_cassette_record_mode is deprecated.  Instead, use: "default_cassette_options = { :record => :#{mode.to_s} }"})
-          VCR::Config.default_cassette_record_mode = mode
         end
       end
     end
