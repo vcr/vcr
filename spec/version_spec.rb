5226f81595318b868899f1c8b91a43ae438632e8
Myron Marston <myron.marston@gmail.com>
2010-07-01 14:30:50 -0700

    Fixed spec description.

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "VCR Version" do
+describe "VCR.version" do
   subject { VCR.version }
 
   it { should =~ /\A\d+\.\d+\.\d+\z/ }
