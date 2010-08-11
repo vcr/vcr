788590f725fdbd2bb2164e43fa156358b1a2d84d
Myron Marston <myron.marston@gmail.com>
2010-08-10 17:57:31 -0700

    Simplify Net::HTTP extension a bit.

--- lib/vcr/extensions/net_http.rb
+++ lib/vcr/extensions/net_http.rb
@@ -2,7 +2,9 @@ require 'net/http'
 
 module Net
   class HTTP
-    def request_with_vcr(request, body = nil, &block)
+    alias_method :request_without_vcr, :request
+
+    def request(request, body = nil, &block)
       uri = URI.parse(VCR.http_stubbing_adapter.request_uri(self, request))
 
       if VCR::LOCALHOST_ALIASES.include?(uri.host) && VCR.http_stubbing_adapter.ignore_localhost?
@@ -14,16 +16,12 @@ module Net
       response = request_without_vcr(request, body)
 
       if started? && !VCR.http_stubbing_adapter.request_stubbed?(request.method.downcase.to_sym, uri)
-        http_interaction = VCR::HTTPInteraction.from_net_http_objects(self, request, response)
+        VCR.record_http_interaction VCR::HTTPInteraction.from_net_http_objects(self, request, response)
         response.extend VCR::Net::HTTPResponse # "unwind" the response
-
-        VCR.record_http_interaction(http_interaction)
       end
 
       yield response if block_given?
       response
     end
-    alias_method :request_without_vcr, :request
-    alias_method :request, :request_with_vcr
   end
 end
