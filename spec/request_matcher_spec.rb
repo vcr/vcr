9fbae9ffc9614cde81ebd21f6db25f73c25397c9
Myron Marston <myron.marston@gmail.com>
2010-08-21 23:37:52 -0700

    Require spec_helper's full expanded path so it's not loaded twice.

--- spec/request_matcher_spec.rb
+++ spec/request_matcher_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe VCR::RequestMatcher do
   it 'raises an error when given invalid match attributes' do
