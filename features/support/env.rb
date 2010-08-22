81957d258e55522d919c16503ecf2471bb62c3ab
Myron Marston <myron.marston@gmail.com>
2010-08-21 21:23:39 -0700

    1.9.2 compatibility fix: don't try to use ruby-debug on 1.9.2.
    On 1.9.2, I'm getting errors like these:
    
    dyld: lazy symbol binding failed: Symbol not found: _rb_method_node
      Referenced from: /Users/mmarston/.rvm/gems/ruby-1.9.2-p0/gems/ruby-debug-base19-0.11.23/lib/ruby_debug.bundle
      Expected in: flat namespace
    
    dyld: Symbol not found: _rb_method_node
      Referenced from: /Users/mmarston/.rvm/gems/ruby-1.9.2-p0/gems/ruby-debug-base19-0.11.23/lib/ruby_debug.bundle
      Expected in: flat namespace

--- features/support/env.rb
+++ features/support/env.rb
@@ -32,7 +32,7 @@ begin
   Debugger.settings[:autoeval] = true if Debugger.respond_to?(:settings)
 rescue LoadError
   # ruby-debug wasn't available so neither can the debugging be
-end
+end unless RUBY_VERSION > '1.9.1' # ruby-debug doesn't work on 1.9.2 yet
 
 # Ruby 1.9.1 has a different yaml serialization format.
 YAML_SERIALIZATION_VERSION = RUBY_VERSION == '1.9.1' ? '1.9.1' : 'not_1.9.1'
