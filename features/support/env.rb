68295fe6133491232f011d7554ec79763bbcf09c
Myron Marston <myron.marston@gmail.com>
2010-11-09 07:52:05 -0800

    Symlink the vcr_cucumber_helpers.rb file for each scenario rather than create it new.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,24 +5,8 @@ Bundler.setup
 require 'aruba'
 
 Before do
-  create_file("vcr_cucumber_helpers.rb", <<-EOF)
-    $LOAD_PATH.unshift '../../spec' unless $LOAD_PATH.include?('../../spec')
-
-    def include_http_adapter_for(lib)
-      require 'support/http_library_adapters'
-      require lib
-      include HTTP_LIBRARY_ADAPTERS[lib]
-    end
-
-    def start_sinatra_app(options, &block)
-      raise ArgumentError.new("You must pass a port") unless options[:port]
-
-      require 'sinatra'
-      require 'support/vcr_localhost_server'
-      klass = Class.new(Sinatra::Base)
-      klass.class_eval(&block)
-
-      VCR::LocalhostServer.new(klass.new, options[:port])
-    end
-  EOF
+  this_dir = File.dirname(__FILE__)
+  in_current_dir do
+    FileUtils.ln_s File.join(this_dir, 'vcr_cucumber_helpers.rb'), 'vcr_cucumber_helpers.rb'
+  end
 end
---
+++ features/support/vcr_cucumber_helpers.rb
@@ -0,0 +1,20 @@
+# This file gets symlinked into the tmp/aruba directory before
+# each scenario so that it is available to be required in them.
+$LOAD_PATH.unshift '../../spec' unless $LOAD_PATH.include?('../../spec')
+
+def include_http_adapter_for(lib)
+  require 'support/http_library_adapters'
+  require lib
+  include HTTP_LIBRARY_ADAPTERS[lib]
+end
+
+def start_sinatra_app(options, &block)
+  raise ArgumentError.new("You must pass a port") unless options[:port]
+
+  require 'sinatra'
+  require 'support/vcr_localhost_server'
+  klass = Class.new(Sinatra::Base)
+  klass.class_eval(&block)
+
+  VCR::LocalhostServer.new(klass.new, options[:port])
+end
