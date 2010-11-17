3f3d2c8dc5a6b0fe313845499521376505c2745b
Myron Marston <myron.marston@gmail.com>
2010-11-16 17:32:36 -0800

    Ignore em-http-request cukes on rubinius since they are failing and it's not likely a bug in VCR.

--- features/support/http_lib_filters.rb
+++ features/support/http_lib_filters.rb
@@ -5,10 +5,17 @@ Cucumber::Ast::OutlineTable::ExampleRow.class_eval do
   end
 end
 
-if RUBY_VERSION == '1.9.2' || defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+if RUBY_VERSION == '1.9.2'
   # For some reason, the local sinatra server locks up and never exits
   # when using patron on 1.9.2, even though it exits fine during the specs.
   UNSUPPORTED_HTTP_LIBS = %w[ patron ]
+elsif defined?(RUBY_ENGINE) && RUBY_ENGINE == 'rbx'
+  # Patron is freezing up the cukes (as it does on 1.9.2)
+  # I'm not sure why em-http-request isn't working on rbx,
+  # but considering the fact that VCR works with all the other
+  # libs just fine and doesn't do anything for em-http-request,
+  # it's probably a bug in it or rbx...so ignore it, for now.
+  UNSUPPORTED_HTTP_LIBS = %w[ patron em-http-request ]
 elsif RUBY_PLATFORM == 'java'
   # These gems have C extensions and can't install on JRuby.
   UNSUPPORTED_HTTP_LIBS = %w[ typhoeus patron curb em-http-request ]
