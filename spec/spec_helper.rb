2ccf387e3375367f866f37eb3d944e6c49e68e8e
Myron Marston <myron.marston@gmail.com>
2011-01-07 23:21:23 -0800

    Don't debug on 1.9.2.
    We're locked to a version of ruby-debug19 that works on 1.9.1 but not 1.9.2.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,7 @@ RSpec.configure do |config|
   config.extend WebMockMacros
 
   config.color_enabled = true
-  config.debug = RUBY_INTERPRETER == :mri
+  config.debug = RUBY_INTERPRETER == :mri && RUBY_VERSION != '1.9.2'
 
   config.before(:each) do
     VCR.turn_on! unless VCR.turned_on?
