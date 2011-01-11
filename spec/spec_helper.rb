21956cf041303c129f8acddfef0d258eee8165ba
Myron Marston <myron.marston@gmail.com>
2011-01-10 19:26:38 -0800

    Simplify require line for spec support files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,7 @@ require 'rspec'
 # Ruby 1.9.1 has a different yaml serialization format.
 YAML_SERIALIZATION_VERSION = RUBY_VERSION == '1.9.1' ? '1.9.1' : 'not_1.9.1'
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require File.join("support", File.basename(f))}
+Dir['./spec/support/**/*.rb'].each { |f| require f }
 
 require 'vcr'
 require 'monkey_patches'
