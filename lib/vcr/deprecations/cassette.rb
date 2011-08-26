735500c0381a794dbf0c763782214d8c67e42580
Myron Marston <myron.marston@gmail.com>
2011-08-25 23:05:08 -0700

    Fix mis-matched indentation warning.

--- lib/vcr/deprecations/cassette.rb
+++ lib/vcr/deprecations/cassette.rb
@@ -5,25 +5,25 @@ module VCR
       VCR::Config.uri_should_be_ignored?(uri.to_s)
     end
 
-    private
+  private
 
-      def deprecate_old_cassette_options(options)
-        message = "VCR's :allow_real_http cassette option is deprecated.  Instead, use the ignore_localhost configuration option."
-        if options[:allow_real_http] == :localhost
-          @original_ignored_hosts = VCR::Config.ignored_hosts.dup
-          VCR::Config.ignored_hosts.clear
-          VCR::Config.ignore_hosts(*VCR::LOCALHOST_ALIASES)
-          Kernel.warn "WARNING: #{message}"
-        elsif options[:allow_real_http]
-          raise ArgumentError.new(message)
-        end
-      end
-
-      def restore_ignore_localhost_for_deprecation
-        if defined?(@original_ignored_hosts)
-          VCR::Config.ignored_hosts.clear
-          VCR::Config.ignore_hosts(*@original_ignored_hosts)
-        end
+    def deprecate_old_cassette_options(options)
+      message = "VCR's :allow_real_http cassette option is deprecated.  Instead, use the ignore_localhost configuration option."
+      if options[:allow_real_http] == :localhost
+        @original_ignored_hosts = VCR::Config.ignored_hosts.dup
+        VCR::Config.ignored_hosts.clear
+        VCR::Config.ignore_hosts(*VCR::LOCALHOST_ALIASES)
+        Kernel.warn "WARNING: #{message}"
+      elsif options[:allow_real_http]
+        raise ArgumentError.new(message)
       end
     end
+
+    def restore_ignore_localhost_for_deprecation
+      if defined?(@original_ignored_hosts)
+        VCR::Config.ignored_hosts.clear
+        VCR::Config.ignore_hosts(*@original_ignored_hosts)
+      end
+    end
+  end
 end
