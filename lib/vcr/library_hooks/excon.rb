b63b3a47f8bdf174bf2be5ae33b3b77f29ccb471
Myron Marston <myron.marston@gmail.com>
2011-11-27 23:02:12 -0800

    Revert "Use latest excon."
    This reverts commit 6e13de0244ee65138a2e0e3da0059cb89d56736f.
    
    It worked for me locally but it broke the Travis build :(.

--- lib/vcr/library_hooks/excon.rb
+++ lib/vcr/library_hooks/excon.rb
@@ -2,7 +2,7 @@ require 'vcr/util/version_checker'
 require 'vcr/request_handler'
 require 'excon'
 
-VCR::VersionChecker.new('Excon', Excon::VERSION, '0.6.5', '0.7').check_version!
+VCR::VersionChecker.new('Excon', Excon::VERSION, '0.6.5', '0.6').check_version!
 
 module VCR
   class LibraryHooks
--- vcr.gemspec
+++ vcr.gemspec
@@ -33,7 +33,7 @@ Gem::Specification.new do |s|
 
     'faraday'         => '~> 0.7.2',
     'httpclient'      => '~> 2.1.5.2',
-    'excon'           => '~> 0.7.8',
+    'excon'           => '~> 0.6.5',
 
     'timecop'         => '~> 0.3.5',
     'rack'            => '1.1.0',
