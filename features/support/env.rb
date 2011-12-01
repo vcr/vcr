1b36333666d207b05dba8674058f82a0b9d4f13b
Myron Marston <myron.marston@gmail.com>
2011-11-30 22:16:02 -0800

    Simplify ENV vars for cukes.

--- features/support/env.rb
+++ features/support/env.rb
@@ -6,26 +6,25 @@ require 'ruby-debug' if !defined?(RUBY_ENGINE) && RUBY_VERSION != '1.9.3' && !EN
 
 require 'aruba/cucumber'
 
-cucumer_helpers_file = '../../features/support/vcr_cucumber_helpers'
-if RUBY_VERSION > '1.9.1'
-  Before do
-    set_env('RUBYOPT', "-I.:../../lib -r#{cucumer_helpers_file}")
-  end
-elsif RUBY_PLATFORM == 'java'
-  Before do
-    set_env('RUBYOPT', "-I../../lib -rubygems -r#{cucumer_helpers_file}")
+Before do
+  load_paths, requires = ['../../lib'], []
 
+  if RUBY_VERSION < '1.9'
+    requires << "rubygems"
+  else
+    load_paths << '.'
+  end
+
+  requires << '../../features/support/vcr_cucumber_helpers'
+  requires.map! { |r| "-r#{r}" }
+  set_env('RUBYOPT', "-I#{load_paths.join(':')} #{requires.join(' ')}")
+
+  if RUBY_PLATFORM == 'java'
     # ideas taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
     set_env('JRUBY_OPTS', '-X-C') # disable JIT since these processes are so short lived
     set_env('JAVA_OPTS', '-d32') # force jRuby to use client JVM for faster startup times
   end
-else
-  Before do
-    set_env('RUBYOPT', "-rubygems -r#{cucumer_helpers_file}")
-  end
-end
 
-Before do
   @aruba_timeout_seconds = RUBY_PLATFORM == 'java' ? 60 : 20
 end
 
