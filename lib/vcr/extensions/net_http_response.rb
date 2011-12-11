42fc56abe29f0385d1400b8e944c7565143cf250
Myron Marston <myron.marston@gmail.com>
2011-12-10 16:26:54 -0800

    Move comment so its not picked up by yard as the VCR overview.

--- lib/vcr/extensions/net_http_response.rb
+++ lib/vcr/extensions/net_http_response.rb
@@ -1,16 +1,16 @@
-# A Net::HTTP response that has already been read raises an IOError when #read_body
-# is called with a destination string or block.
-#
-# This causes a problem when VCR records a response--it reads the body before yielding
-# the response, and if the code that is consuming the HTTP requests uses #read_body, it
-# can cause an error.
-#
-# This is a bit of a hack, but it allows a Net::HTTP response to be "re-read"
-# after it has aleady been read.  This attemps to preserve the behavior of
-# #read_body, acting just as if it had never been read.
-
 module VCR
   module Net
+    # A Net::HTTP response that has already been read raises an IOError when #read_body
+    # is called with a destination string or block.
+    #
+    # This causes a problem when VCR records a response--it reads the body before yielding
+    # the response, and if the code that is consuming the HTTP requests uses #read_body, it
+    # can cause an error.
+    #
+    # This is a bit of a hack, but it allows a Net::HTTP response to be "re-read"
+    # after it has aleady been read.  This attemps to preserve the behavior of
+    # #read_body, acting just as if it had never been read.
+    # @private
     module HTTPResponse
       def self.extended(response)
         response.instance_variable_set(:@__read_body_previously_called, false)
