a6648655d0a828280f21ce2e0896b40790804c81
Myron Marston <myron.marston@gmail.com>
2011-12-18 22:38:10 -0800

    Document the structs.

--- lib/vcr/structs.rb
+++ lib/vcr/structs.rb
@@ -4,6 +4,7 @@ require 'forwardable'
 module VCR
   # @private
   module Normalizers
+    # @private
     module Body
       def initialize(*args)
         super
@@ -16,6 +17,7 @@ module VCR
       end
     end
 
+    # @private
     module Header
       def initialize(*args)
         super
@@ -57,6 +59,7 @@ module VCR
     end
   end
 
+  # @private
   module OrderedHashSerializer
     def each
       @ordered_keys.each do |key|
@@ -75,6 +78,12 @@ module VCR
     end
   end
 
+  # The request of an {HTTPInteraction}.
+  #
+  # @attr [Symbol] method the HTTP method (i.e. :head, :options, :get, :post, :put, :patch or :delete)
+  # @attr [String] uri the request URI
+  # @attr [String, nil] body the request body
+  # @attr [Hash{String => Array<String>}] headers the request headers
   class Request < Struct.new(:method, :uri, :body, :headers)
     include Normalizers::Header
     include Normalizers::Body
@@ -85,6 +94,11 @@ module VCR
       self.uri = without_standard_port(self.uri)
     end
 
+    # Builds a serializable hash from the request data.
+    #
+    # @return [Hash] hash that represents this request and can be easily
+    #  serialized.
+    # @see Request.from_hash
     def to_hash
       {
         'method'  => method.to_s,
@@ -94,6 +108,10 @@ module VCR
       }.tap { |h| OrderedHashSerializer.apply_to(h, members) }
     end
 
+    # Constructs a new instance from a hash.
+    #
+    # @param [Hash] hash the hash to use to construct the instance.
+    # @return [Request] the request
     def self.from_hash(hash)
       method = hash['method']
       method &&= method.to_sym
@@ -109,16 +127,30 @@ module VCR
       @@object_method.bind(self).call(*args)
     end
 
-    # transforms the request into a fiber aware one
+    # Transforms the request into a fiber aware one by extending
+    # the {FiberAware} module onto the instance. Necessary for the
+    # {VCR::Configuration#around_http_request} hook.
+    #
+    # @return [Request] the request instance
     def fiber_aware
       extend FiberAware
     end
 
+    # Designed to be extended onto a {VCR::Request} instance in order to make
+    # it fiber-aware for the {VCR::Configuration#around_http_request} hook.
     module FiberAware
+      # Yields the fiber so the request can proceed.
+      #
+      # @return [VCR::Response] the response from the request
       def proceed
         Fiber.yield
       end
 
+      # Builds a proc that allows the request to proceed when called.
+      # This allows you to treat the request as a proc and pass it on
+      # to a method that yields (at which point the request will proceed).
+      #
+      # @return [Proc] the proc
       def to_proc
         lambda { proceed }
       end
@@ -135,6 +167,11 @@ module VCR
     end
   end
 
+  # Represents a single interaction over HTTP, containing a request and a response.
+  #
+  # @attr [Request] request the request
+  # @attr [Response] response the response
+  # @attr [Time] recorded_at when this HTTP interaction was recorded
   class HTTPInteraction < Struct.new(:request, :response, :recorded_at)
     def initialize(*args)
       @ignored = false
@@ -142,6 +179,11 @@ module VCR
       self.recorded_at ||= Time.now
     end
 
+    # Builds a serializable hash from the HTTP interaction data.
+    #
+    # @return [Hash] hash that represents this HTTP interaction
+    #  and can be easily serialized.
+    # @see HTTPInteraction.from_hash
     def to_hash
       {
         'request'     => request.to_hash,
@@ -152,20 +194,35 @@ module VCR
       end
     end
 
+    # Constructs a new instance from a hash.
+    #
+    # @param [Hash] hash the hash to use to construct the instance.
+    # @return [HTTPInteraction] the HTTP interaction
     def self.from_hash(hash)
       new Request.from_hash(hash.fetch('request', {})),
           Response.from_hash(hash.fetch('response', {})),
           Time.httpdate(hash.fetch('recorded_at'))
     end
 
+    # Flags the HTTP interaction so that VCR ignores it. This is useful in
+    # a {VCR::Configuration#before_record} or {VCR::Configuration#before_playback}
+    # hook so that VCR does not record or play it back.
+    # @see #ignored?
     def ignore!
       @ignored = true
     end
 
+    # @return [Boolean] whether or not this HTTP interaction should be ignored.
+    # @see #ignore!
     def ignored?
       !!@ignored
     end
 
+    # Replaces a string in any part of the HTTP interaction (headers, request body,
+    # response body, etc) with the given replacement text.
+    #
+    # @param [String] text the text to replace
+    # @param [String] replacement_text the text to put in its place
     def filter!(text, replacement_text)
       return self if [text, replacement_text].any? { |t| t.to_s.empty? }
       filter_object!(self, text, replacement_text)
@@ -196,10 +253,21 @@ module VCR
     end
   end
 
+  # The response of an {HTTPInteraction}.
+  #
+  # @attr [ResponseStatus] status the status of the response
+  # @attr [Hash{String => Array<String>}] headers the response headers
+  # @attr [String] body the response body
+  # @attr [nil, String] http_version the HTTP version
   class Response < Struct.new(:status, :headers, :body, :http_version)
     include Normalizers::Header
     include Normalizers::Body
 
+    # Builds a serializable hash from the response data.
+    #
+    # @return [Hash] hash that represents this response
+    #  and can be easily serialized.
+    # @see Response.from_hash
     def to_hash
       {
         'status'       => status.to_hash,
@@ -209,6 +277,10 @@ module VCR
       }.tap { |h| OrderedHashSerializer.apply_to(h, members) }
     end
 
+    # Constructs a new instance from a hash.
+    #
+    # @param [Hash] hash the hash to use to construct the instance.
+    # @return [Response] the response
     def self.from_hash(hash)
       new ResponseStatus.from_hash(hash.fetch('status', {})),
           hash['headers'],
@@ -216,6 +288,8 @@ module VCR
           hash['http_version']
     end
 
+    # Updates the Content-Length response header so that it is
+    # accurate for the response body.
     def update_content_length_header
       value = body ? body.bytesize.to_s : '0'
       key = %w[ Content-Length content-length ].find { |k| headers.has_key?(k) }
@@ -223,13 +297,26 @@ module VCR
     end
   end
 
+  # The response status of an {HTTPInteraction}.
+  #
+  # @attr [Integer] code the HTTP status code
+  # @attr [String] message the HTTP status message (e.g. "OK" for a status of 200)
   class ResponseStatus < Struct.new(:code, :message)
+    # Builds a serializable hash from the response status data.
+    #
+    # @return [Hash] hash that represents this response status
+    #  and can be easily serialized.
+    # @see ResponseStatus.from_hash
     def to_hash
       {
         'code' => code, 'message' => message
       }.tap { |h| OrderedHashSerializer.apply_to(h, members) }
     end
 
+    # Constructs a new instance from a hash.
+    #
+    # @param [Hash] hash the hash to use to construct the instance.
+    # @return [ResponseStatus] the response status
     def self.from_hash(hash)
       new hash['code'], hash['message']
     end
