c4f44f813e2a0b0f02c48900fcdf793013631453
Myron Marston <myron.marston@gmail.com>
2012-01-27 22:31:39 -0800

    Move simplecov config into spec_helper.

--- .simplecov
+++
@@ -1,9 +0,0 @@
-SimpleCov.start do
-  add_filter "/spec"
-  add_filter "/features"
-
-  # internet_connection mostly contains logic copied from the ruby 1.8.7
-  # stdlib for which I haven't written tests.
-  add_filter "internet_connection"
-end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'simplecov'
 
+SimpleCov.start do
+  add_filter "/spec"
+  add_filter "/features"
+
+  # internet_connection mostly contains logic copied from the ruby 1.8.7
+  # stdlib for which I haven't written tests.
+  add_filter "internet_connection"
+end
+
 using_git = File.exist?(File.expand_path('../../.git/', __FILE__))
 if using_git
   require 'bundler'
