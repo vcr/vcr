b37e5939ff0bb5274ede3e76b343de4041c701f7
Myron Marston <myron.marston@gmail.com>
2012-01-27 21:13:47 -0800

    Remove env vars that aruba now sets for JRuby.

--- features/support/env.rb
+++ features/support/env.rb
@@ -24,12 +24,6 @@ Before do
   requires.map! { |r| "-r#{r}" }
   set_env('RUBYOPT', "-I#{load_paths.join(':')} #{requires.join(' ')}")
 
-  if RUBY_PLATFORM == 'java'
-    # ideas taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
-    set_env('JRUBY_OPTS', '-X-C') # disable JIT since these processes are so short lived
-    set_env('JAVA_OPTS', '-d32') # force jRuby to use client JVM for faster startup times
-  end
-
   if additional_paths.any?
     existing_paths = ENV['PATH'].split(':')
     set_env('PATH', (additional_paths + existing_paths).join(':'))
