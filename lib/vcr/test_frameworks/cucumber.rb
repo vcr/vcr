55b012f3983ccf8b4dbf55607271b94c504414f9
Omer Rauchwerger <rauchy@gmail.com>
2012-03-04 17:35:59 +0200

    yard documentation

--- lib/vcr/test_frameworks/cucumber.rb
+++ lib/vcr/test_frameworks/cucumber.rb
@@ -24,7 +24,7 @@ module VCR
     # will cause a VCR cassette to be used for scenarios with matching tags.
     #
     # @param [Array<String>] tag_names the cucumber scenario tags
-    # @param [(optional) Hash] options the cassette options
+    # @param [(optional) Hash] options the cassette options. Specify :use_scenario_name => true to automatically name the cassette according to the scenario name.
     def tags(*tag_names)
       options = tag_names.last.is_a?(::Hash) ? tag_names.pop : {}
       tag_names.each do |tag_name|
