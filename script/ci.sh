49718c3d790f873de9eb62317a8eb7e880e4b522
Myron Marston <myron.marston@gmail.com>
2012-10-11 23:10:27 -0700

    Put Typhoeus 0.4 specs first.
    The coverage check runs at the end and should run against the code coverage from the full spec build.

--- script/ci.sh
+++ script/ci.sh
@@ -1,16 +1,16 @@
 # Kill the whole script on error
 set -e
 
+echo "-------- Running Typhoeus 0.4 Specs ---------"
+bundle install --gemfile=gemfiles/typhoeus-old.gemfile
+BUNDLE_GEMFILE=gemfiles/typhoeus-old.gemfile bundle exec rspec spec/vcr/library_hooks/typhoeus_0.4_spec.rb --format progress --backtrace
+
 # Setup vendored rspec-1
 git submodule init
 git submodule update
 
 echo "-------- Running Specs ---------"
 bundle exec ruby -w -I./spec -r./spec/capture_warnings -rspec_helper -S rspec spec --format progress --backtrace
-
-echo "-------- Running Typhoeus 0.4 Specs ---------"
-bundle install --gemfile=gemfiles/typhoeus-old.gemfile
-BUNDLE_GEMFILE=gemfiles/typhoeus-old.gemfile bundle exec rspec spec/vcr/library_hooks/typhoeus_0.4_spec.rb --format progress --backtrace
 
 echo "-------- Running Cukes ---------"
 bundle exec cucumber
