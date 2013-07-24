5bbb29ccfe5097340c89087ec2ae68d020f78de3
Luan Santos and Matt Parker <pair+luan+mparker@pivotallabs.com>
2013-07-24 12:01:03 -0700

    Don't try to log when there is no logger

--- lib/vcr/util/logger.rb
+++ lib/vcr/util/logger.rb
@@ -2,16 +2,19 @@ module VCR
   # @private
   module Logger
     def log(message, indentation_level = 0)
+      return unless logger
       indentation = '  ' * indentation_level
       log_message = indentation + log_prefix + message
-      VCR.configuration.debug_logger.puts log_message
+      logger.puts log_message
     end
 
     def log_prefix
+      return unless logger
       ''
     end
 
     def request_summary(request, request_matchers)
+      return unless logger
       attributes = [request.method, request.uri]
       attributes << request.body.to_s[0, 80].inspect if request_matchers.include?(:body)
       attributes << request.headers.inspect          if request_matchers.include?(:headers)
@@ -19,7 +22,12 @@ module VCR
     end
 
     def response_summary(response)
+      return unless logger
       "[#{response.status.code} #{response.body[0, 80].inspect}]"
+    end
+
+    def logger
+      @logger ||= VCR.configuration.debug_logger
     end
   end
 end
