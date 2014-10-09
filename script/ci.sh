13866a826ea0ed7573677760cf307da8e9141ced
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-10-09 20:30:02 +0200

    Use bash to verify that there were no Ruby syntax warnings

--- script/ci.sh
+++ script/ci.sh
@@ -1,5 +1,24 @@
-# Kill the whole script on error
-set -e -x
+#!/bin/bash
+set -e
+
+STATUS=0
+warnings="${TMPDIR:-/tmp}/vcr-warnings.$$"
+
+run() {
+  # Save warnings on stderr to a separate file
+  RUBYOPT="$RUBYOPT -w" bundle exec "$@" \
+    2> >(tee >(grep 'warning:' >>"$warnings") | grep -v 'warning:') || STATUS=$?
+}
+
+check_warnings() {
+  # Display Ruby warnings from this project's source files. Abort if any were found.
+  num="$(grep -F "$PWD" "$warnings" | grep -v "${PWD}/vendor/bundle" | sort | uniq -c | sort -rn | tee /dev/stderr | wc -l)"
+  rm -f "$warnings"
+  if [ "$num" -gt 0 ]; then
+    echo "FAILED: this test suite doesn't tolerate Ruby syntax warnings!" >&2
+    exit 1
+  fi
+}
 
 # idea taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
 export JRUBY_OPTS='-X-C' # disable JIT since these processes are so short lived
@@ -9,22 +28,26 @@ export JRUBY_OPTS='-X-C' # disable JIT since these processes are so short lived
 export JAVA_OPTS='-client -XX:+TieredCompilation -XX:TieredStopAtLevel=1'
 
 echo "-------- Running Typhoeus 0.4 Specs ---------"
-BUNDLE_GEMFILE=gemfiles/typhoeus_old.gemfile bundle exec rspec spec/vcr/library_hooks/typhoeus_0.4_spec.rb --format progress --backtrace
+BUNDLE_GEMFILE=gemfiles/typhoeus_old.gemfile run rspec spec/vcr/library_hooks/typhoeus_0.4_spec.rb
 
 echo "-------- Running Faraday 0.8 Specs ---------"
-BUNDLE_GEMFILE=gemfiles/faraday_old.gemfile bundle exec rspec spec/vcr/middleware/faraday_spec.rb spec/vcr/library_hooks/faraday_spec.rb --format progress --backtrace
-BUNDLE_GEMFILE=gemfiles/faraday_old.gemfile bundle exec cucumber features/middleware/faraday.feature --format progress --backtrace
+BUNDLE_GEMFILE=gemfiles/faraday_old.gemfile run rspec spec/vcr/middleware/faraday_spec.rb spec/vcr/library_hooks/faraday_spec.rb
+BUNDLE_GEMFILE=gemfiles/faraday_old.gemfile run cucumber features/middleware/faraday.feature
 
 # Setup vendored rspec-1
 bundle exec rake submodules
 
 echo "-------- Running Specs ---------"
-bundle exec ruby -I./spec -r./spec/capture_warnings -rspec_helper -S rspec spec --format progress --backtrace
+run rspec
 
 echo "-------- Running Cukes ---------"
-bundle exec cucumber
+run cucumber
 
 echo "-------- Checking Coverage ---------"
 bundle exec rake yard_coverage
 
 bundle exec rake check_code_coverage
+
+check_warnings
+
+exit $STATUS
