040aaac5370c68cd13c847c076749cd547a6f9b1
Mislav Marohnić <mislav.marohnic@gmail.com>
2014-10-10 09:05:10 +0200

    Output timing information per test operation in Travis web interface

--- script/ci.sh
+++ script/ci.sh
@@ -4,16 +4,40 @@ set -e
 STATUS=0
 warnings="${TMPDIR:-/tmp}/vcr-warnings.$$"
 
+nano_cmd="$(type -p gdate date | head -1)"
+nano_format="+%s%N"
+[ "$(uname -s)" != "Darwin" ] || nano_format="${nano_format/%N/000000000}"
+
+travis_time_start() {
+  travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
+  travis_start_time=$($nano_cmd -u "$nano_format")
+  printf "travis_time:start:%s\r\e[0m" $travis_timer_id
+}
+
+travis_time_finish() {
+  local travis_end_time=$($nano_cmd -u "$nano_format")
+  local duration=$(($travis_end_time-$travis_start_time))
+  printf "travis_time:end:%s:start=%s,finish=%s,duration=%s\r\e[0m" \
+    $travis_timer_id $travis_start_time $travis_end_time $duration
+}
+
 fold() {
   local name="$1"
   local status=0
   shift 1
-  [ -z "$TRAVIS" ] || printf "travis_fold:start:%s\r" "$name"
+  if [ -n "$TRAVIS" ]; then
+    printf "travis_fold:start:%s\r\e[0m" "$name"
+    travis_time_start
+  fi
 
   "$@" || status=$?
 
+  [ -z "$TRAVIS" ] || travis_time_finish
+
   if [ "$status" -eq 0 ]; then
-    [ -z "$TRAVIS" ] || printf "travis_fold:end:%s\r" "$name"
+    if [ -n "$TRAVIS" ]; then
+      printf "travis_fold:end:%s\r\e[0m" "$name"
+    fi
   else
     STATUS="$status"
   fi
@@ -33,6 +57,8 @@ check_warnings() {
     exit 1
   fi
 }
+
+trap 'exit 1' INT
 
 # idea taken from: http://blog.headius.com/2010/03/jruby-startup-time-tips.html
 export JRUBY_OPTS='-X-C' # disable JIT since these processes are so short lived
