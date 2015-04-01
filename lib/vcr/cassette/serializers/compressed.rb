904ae5053d7231de550c005fd0af746121020e18
Ben Lavender <bhuga@github.com>
2015-04-01 03:35:16 -0300

    Implement a compressed cassette format

---
+++ lib/vcr/cassette/serializers/compressed.rb
@@ -0,0 +1,45 @@
+require 'zlib'
+require 'vcr/cassette/serializers'
+
+module VCR
+  class Cassette
+    class Serializers
+      # The compressed serializer. This serializer wraps the YAML serializer
+      # to write compressed cassettes to disk.
+      #
+      # JSON cassettes often compress at greater than 10:1. The tradeoff for
+      # this is that cassettes will not diff nicely or be easily inspectable or
+      # editable.
+      #
+      # @see YAML
+      module Compressed
+        extend self
+
+        # The file extension to use for this serializer.
+        #
+        # @return [String] "gz"
+        def file_extension
+          'gz'
+        end
+
+        # Serializes the given hash using YAML and Zlib.
+        #
+        # @param [Hash] hash the object to serialize
+        # @return [String] the compressed cassette data
+        def serialize(hash)
+          string = VCR::Cassette::Serializers::YAML.serialize(hash)
+          Zlib.deflate(string)
+        end
+
+        # Deserializes the given compressed cassette data.
+        #
+        # @param [String] string the compressed YAML cassette data
+        # @return [Hash] the deserialized object
+        def deserialize(string)
+          yaml = Zlib.inflate(string)
+          VCR::Cassette::Serializers::YAML.deserialize(yaml)
+        end
+      end
+    end
+  end
+end
