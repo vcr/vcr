c4543a71d402b53ec92755b0c0636f89583c5158
Jan Berdajs <mrbrdo@gmail.com>
2015-10-11 14:12:01 +0200

    Fix spec warning

--- spec/lib/vcr_spec.rb
+++ spec/lib/vcr_spec.rb
@@ -100,7 +100,8 @@ describe VCR do
 
     it 'ejects the cassette even if there is an error' do
       expect(VCR).to receive(:eject_cassette)
-      expect { VCR.use_cassette(:cassette_test) { raise StandardError } }.to raise_error
+      test_error = Class.new(StandardError)
+      expect { VCR.use_cassette(:cassette_test) { raise test_error } }.to raise_error(test_error)
     end
 
     it 'does not eject a cassette if there was an error inserting it' do
