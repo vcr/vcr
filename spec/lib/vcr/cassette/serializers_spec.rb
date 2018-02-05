fa9fedccc6c7d3d5f9460f092fc719516c2ac6b3
KARASZI István <github@spam.raszi.hu>
2018-02-05 12:34:47 +0100

    Follow code changes in tests

--- spec/lib/vcr/cassette/serializers_spec.rb
+++ spec/lib/vcr/cassette/serializers_spec.rb
@@ -66,7 +66,7 @@ module VCR
         end if ''.respond_to?(:encoding)
       end if RUBY_VERSION =~ /1.9/
 
-      it_behaves_like "a serializer", :compressed, "gz",  :lazily_loaded do
+      it_behaves_like "a serializer", :compressed, "zz",  :lazily_loaded do
         it_behaves_like "encoding error handling", :compressed, ArgumentError do
           let(:string) { "\xFA".force_encoding("UTF-8") }
         end if ''.respond_to?(:encoding)
