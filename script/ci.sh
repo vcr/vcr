7a97999ae52011805c630c0cda05ad0e9d1c5887
Jimmy Bradshaw <james.g.bradshaw@gmail.com>
2021-02-27 23:13:29 -0600

    Migrate from TravisCI to GitHub Actions

--- script/ci.sh
+++ script/ci.sh
@@ -4,39 +4,19 @@ set -e
 STATUS=0
 warnings="${TMPDIR:-/tmp}/vcr-warnings.$$"
 
-nano_cmd="$(type -p gdate date | head -1)"
-nano_format="+%s%N"
-[ "$(uname -s)" != "Darwin" ] || nano_format="${nano_format/%N/000000000}"
-
-travis_time_start() {
-  travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
-  travis_start_time=$($nano_cmd -u "$nano_format")
-  printf "travis_time:start:%s\r\e[0m" $travis_timer_id
-}
-
-travis_time_finish() {
-  local travis_end_time=$($nano_cmd -u "$nano_format")
-  local duration=$(($travis_end_time-$travis_start_time))
-  printf "travis_time:end:%s:start=%s,finish=%s,duration=%s\r\e[0m" \
-    $travis_timer_id $travis_start_time $travis_end_time $duration
-}
-
 fold() {
   local name="$1"
   local status=0
   shift 1
-  if [ -n "$TRAVIS" ]; then
-    printf "travis_fold:start:%s\r\e[0m" "$name"
-    travis_time_start
+  if [ -n "$CI" ]; then
+    echo "::group::${name}"
   fi
 
   "$@" || status=$?
 
-  [ -z "$TRAVIS" ] || travis_time_finish
-
   if [ "$status" -eq 0 ]; then
-    if [ -n "$TRAVIS" ]; then
-      printf "travis_fold:end:%s\r\e[0m" "$name"
+    if [ -n "$CI" ]; then
+      echo "::endgroup::"
     fi
   else
     STATUS="$status"
@@ -47,7 +27,6 @@ fold() {
 }
 
 run() {
-  [[ "${BUNDLE_GEMFILE##*/}" == Gemfile.* ]] && bundle install
   # Save warnings on stderr to a separate file
   RUBYOPT="$RUBYOPT -w" "$@" 2> >(tee >(grep 'warning:' >>"$warnings") | grep -v 'warning:')
 }
