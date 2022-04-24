717b5d1253f67f4d9beefd0743e0fe90a17ca1d4
Nelli Khisamova <73402919+nellirx@users.noreply.github.com>
2022-04-24 21:10:02 +0100

    Remove redundant test

--- spec/lib/vcr/library_hooks/typhoeus_spec.rb
+++ spec/lib/vcr/library_hooks/typhoeus_spec.rb
@@ -193,24 +193,6 @@ RSpec.describe "Typhoeus hook", :with_monkey_patches => :typhoeus, :if => (RUBY_
     end
   end
 
-  context 'when using on_body callback returning :abort' do
-    def make_request
-      VCR.use_cassette('no_body') do
-        request = Typhoeus::Request.new("http://localhost:#{VCR::SinatraApp.port}/localhost_test")
-        request.on_body { next :abort }
-        request.run
-      end
-    end
-
-    it 'records and replays correctly' do
-      recorded = make_request
-      played_back = make_request
-
-      expect(recorded.body).to eq('Localhost response')
-      expect(played_back.body).to eq(recorded.body)
-    end
-  end
-
   context '#effective_url' do
     ResponseValues = Struct.new(:status, :body, :effective_url)
 
