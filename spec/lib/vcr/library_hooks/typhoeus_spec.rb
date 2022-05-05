be6a78281ceabc3af7ddd300b0f7c7796886a4ef
Nelli K <73402919+nellirx@users.noreply.github.com>
2022-05-05 16:40:49 +0100

    Clarify typhoeus callbacks tests (#935)
    * Clarify test description to better reflect what it does
    
    * Add missing assertions
    
    * Split tests per callback
    
    Leaving assertions for body in on_headers callback test, because I don't
    yet know if body affected by on_headers callback or not.
    
    * Confirm on_headers doesn't affect response body
    
    Only streaming responses has no body, because those sent in chunks to
    the on_body callback.
    
    * Make expectations more explicit for on_headers callback
    
    * Simplify with single assertion per test
    
    Easier to see action which makes request and therefore requires cassette
    
    * Document expected arguments
    
    * Use consistent matchers to verify response. Regardless of where response occurs, i.e. inside or outside of callback.
    
    * Add more tests covering on_body returning :abort

--- spec/lib/vcr/library_hooks/typhoeus_spec.rb
+++ spec/lib/vcr/library_hooks/typhoeus_spec.rb
@@ -110,47 +110,46 @@ RSpec.describe "Typhoeus hook", :with_monkey_patches => :typhoeus, :if => (RUBY_
     end
   end
 
-  context 'when using on_body callback' do
-    def make_request
-      VCR.use_cassette('no_body') do
-        request = Typhoeus::Request.new("http://localhost:#{VCR::SinatraApp.port}/localhost_test")
-        request.on_headers { on_headers_counter.increment }
-        request.on_body { on_body_counter.increment }
-        request.run
-      end
+  context 'when using on_headers callback' do
+    def on_headers(&callback)
+      callback = Proc.new {} unless block_given?
+
+      VCR.use_cassette('on_headers') { request.tap { |r| r.on_headers(&callback) }.run }
     end
 
-    let(:on_headers_counter) { double(:increment => nil) }
-    let(:on_body_counter) { double(:increment => nil) }
+    let(:request) { Typhoeus::Request.new("http://localhost:#{VCR::SinatraApp.port}/localhost_test") }
 
-    it 'records and replays correctly' do
-      expect(on_headers_counter).to receive(:increment).exactly(2).times
-      expect(on_body_counter).to receive(:increment).exactly(2).times
+    it { expect(request.tap { |r| r.on_headers {} }).not_to be_streaming }
 
-      recorded = make_request
-      played_back = make_request
+    it { expect { |b| on_headers(&b) }.to yield_with_args(have_attributes(headers: include('Content-Length' => '18'))) }
+    it { expect { |b| on_headers(&b) }.to yield_with_args(have_attributes(headers: match_array(on_headers.headers))) }
 
-      expect(recorded.body).to eq('Localhost response')
-      expect(played_back.body).to eq(recorded.body)
-    end
+    it { expect(on_headers).to have_attributes(headers: include("Content-Length" => "18")) }
+    it { expect(on_headers).to have_attributes(headers: match_array(on_headers.headers))  }
   end
 
-  context 'when using on_body callback returning :abort' do
-    def make_request
-      VCR.use_cassette('no_body') do
-        request = Typhoeus::Request.new("http://localhost:#{VCR::SinatraApp.port}/localhost_test")
-        request.on_body { next :abort }
-        request.run
-      end
+  context 'when using on_body callback' do
+    def on_body(&callback)
+      callback = Proc.new {} unless block_given?
+
+      VCR.use_cassette('no_body') { request.tap { |r| r.on_body(&callback) }.run }
     end
 
-    it 'records and replays correctly' do
-      recorded = make_request
-      played_back = make_request
-
-      expect(recorded.body).to eq('Localhost response')
-      expect(played_back.body).to eq(recorded.body)
+    def request
+      Typhoeus::Request.new("http://localhost:#{VCR::SinatraApp.port}/localhost_test")
     end
+
+    it { expect(request.tap { |r| r.on_body {} }).to be_streaming }
+
+    it { expect(on_body).to have_attributes(body: 'Localhost response') }
+    it { expect(on_body).to have_attributes(body: on_body.body)  }
+
+    it { expect { |b| on_body(&b) }.to yield_with_args('Localhost response', have_attributes(body: '')) }
+    it { expect { |b| on_body(&b) }.to yield_with_args(on_body.body, have_attributes(body: 'Localhost response')) }
+
+    it { expect(on_body { next :abort }).to have_attributes(body: on_body.body)  }
+    it { expect(on_body { next :abort }).to have_attributes(body: on_body { next :abort }.body)  }
+    it { expect(on_body).to have_attributes(body: on_body { next :abort }.body)  }
   end
 
   context '#effective_url' do
